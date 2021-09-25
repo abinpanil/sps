255cccf23e22b1a43da0d01468767e0b0ded3a73
abinpanil007 <abinpanil9@gmail.com>
2021-09-25 19:18:43 +0530

    completed

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function playVideo(params) {
+function playVideo(imageid, iframeid) {
     
+    console.log(imageid);
+    console.log(iframeid);
+    document.getElementById(imageid).style.display = "none";
+    document.getElementById(iframeid).style.display = "block";
+    let id=document.getElementById(iframeid)
+    id.childNodes[1].src+='?autoplay=1'
+   
+    
+
+      
 }
