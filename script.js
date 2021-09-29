4f5eb0cb24718728143351e33e7ac9432bf713c8
abinpanil007 <abinpanil9@gmail.com>
2021-09-29 17:32:43 +0530

    Disign issue Fixed

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 function playVideo(imageid, iframeid) {
     
-    console.log(imageid);
-    console.log(iframeid);
     document.getElementById(imageid).style.display = "none";
     document.getElementById(iframeid).style.display = "block";
     let id=document.getElementById(iframeid)
     id.childNodes[1].src+='?autoplay=1'
-   
-    
-
       
 }
